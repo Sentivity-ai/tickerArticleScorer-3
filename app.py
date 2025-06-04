c2c01989c6486c26cf62cb5d1b0c886e06fd78fc
shravan-del <52248297+shravan-del@users.noreply.github.com>
2025-06-04 11:41:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -132,5 +132,3 @@ def analyze_ticker(ticker: str):
 
     return results                                  
 
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
